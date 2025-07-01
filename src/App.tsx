d4e7e2a407a179101e1f7ca8885633a95e244c93
Hugo <boyer.hugo974@gmail.com>
2025-07-01 23:44:57 +0200

    feat(style) : Style chatbot epurer 3

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { ChatListProvider } from './components/ChatList'
 function App() {
   return (
     <div className="app">
-      <h1>Chatbot avec Ollama</h1>
       <ChatListProvider>
         <Chatbot />
       </ChatListProvider>
